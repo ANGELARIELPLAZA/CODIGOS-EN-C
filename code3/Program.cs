0f2d463b47f916ac83aed51d3f2bd63219e83644
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2022-05-09 12:46:18 -0500

    documentacion

--- code3/Program.cs
+++ code3/Program.cs
@@ -10,19 +10,26 @@ namespace test
     {
         static void Main(string[] args)
         {
-
+            //Imprime mensaje 
             Console.Write("¿Cual es la temperatura actual? ");
+            //lee consola e almacena en variable temp  tipo string
             string temp = Console.ReadLine();
+            //convierte la variable temp de string a flotante
             float value = float.Parse(temp, CultureInfo.InvariantCulture.NumberFormat);
             
+            //evalua value  y compara si es mayor a 20
             if ( value > 20)
-            {
+            { //si es menor a 20 imprime la temperatura
                 Console.WriteLine(value);
+              //imprime el mensaje de que el clima es caluroso
                 Console.Write("El clima es caluroso");           
             }
+            //evalua value  y compara si es menor a 20
             else if ( value < 20)
-            {
+            {   // si  value es menor a 20
+                // imprime la temperatura
                 Console.WriteLine(value);
+                //imprime el mensaje de que el clima es frio
                 Console.Write("El clima es frio");
             }
 
